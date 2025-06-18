0062ad1732a15581f8f07bdfcfa2aa9ab403953e
luispaulohrs <luispaulohr@gmail.com>
2025-06-18 20:45:46 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOIXS27Z9d3QZE96tfEAxr0J4ukWW1MaI",
+  authDomain: "questoeszeca.firebaseapp.com",
+  projectId: "questoeszeca",
+  storageBucket: "questoeszeca.appspot.com",
+  messagingSenderId: "583927044537",
+  appId: "1:583927044537:web:09ca6af330de9720883e68"
+};
+
+initializeApp(firebaseConfig);
